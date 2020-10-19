37da366260b490f6b9d7b84433c6338417cc556f
zodgevaibhav <zodgevaibhav@gmail.com>
2020-10-19 20:53:11 +0530

    Screenshot code added

--- selenium-framework/src/common/IInvokedMethodListenerImpl.java
+++ selenium-framework/src/common/IInvokedMethodListenerImpl.java
@@ -1,5 +1,11 @@
 package common;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.IInvokedMethod;
 import org.testng.IInvokedMethodListener;
@@ -19,10 +25,31 @@ public class IInvokedMethodListenerImpl implements IInvokedMethodListener {
 	
 	
 	@Override
-	public void afterInvocation(IInvokedMethod arg0, ITestResult arg1) {
-
+	public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+		
+		if(!testResult.isSuccess())
+			takeScreenshot(method.getTestMethod().getMethodName()+".png");
+		
 		WebDriverFactory.getDriver().quit();
+		
 	}
 
+	
+	public static void takeScreenshot(String fileNameWithPath) {
+		int a=10;
+		
+		float b = (float)a; //auto boxing  //10.00
 
+		//Decorator Design Patern
+		
+		File scrFile = ((TakesScreenshot) WebDriverFactory.getDriver()).getScreenshotAs(OutputType.FILE);
+		
+		try {
+			FileUtils.copyFile(scrFile, new File(fileNameWithPath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+		
+		
+	}
 }
